3026ef3b0f07d70c81c2fe221413a0267e1845b3
Abhishek Kosuri <kosuri@rockhopper-04.cs.wisc.edu>
2018-10-12 17:54:02 -0500

    queue

--- queue.c
+++ queue.c
@@ -1,1 +1,21 @@
 // queue.c
+//
+//
+typedef struct{
+        int size = 10;
+        int front;
+        int rear;
+}Queue;
+
+Queue *CreateStringQueue(int size){
+	Queue* q = (Queue*)malloc(sizeof(size));
+	q->front = 0;
+        q->rear = -1;
+        return q;
+
+}
+void EnqueueString(Queue *q, char *string){
+	if(q
+}
+char * DequeueString(Queue *q){}
+void PrintQueueStats(Queue *q){}
