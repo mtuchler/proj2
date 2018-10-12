44dc6c445dc9be6c029c6b01756db7de95cce42e
Michael Tuchler <tuchler@royal-05.cs.wisc.edu>
2018-10-12 17:51:40 -0500

    change include

--- threads.h
+++ threads.h
@@ -4,7 +4,10 @@
 //
 // Authors:     Michael Tuchler
 //              Abhishek Kosuri
-// Included in: reader.c, munch1.c, munch2.c, writer.c, main.h
+// Included in: reader.c, munch1.c, munch2.c,
+// 		writer.c, queue.c, main.h
 // Last Edit:   10/12/2018
 //
 /////////////////////////////////
+
+#include <stdio.h>
