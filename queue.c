5869e29d7b7509e912a74d23773cc36de69b6aed
Abhishek Kosuri <kosuri@rockhopper-04.cs.wisc.edu>
2018-10-13 15:39:57 -0500

    comment

--- queue.c
+++ queue.c
@@ -1,6 +1,17 @@
 // queue.c
 //
 //
+#include <stdio.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <unistd.h>
+
+int mutex = 1;
+int wait = 0;
+sem_t mutex;
+sem_t wait;
+
+//queue structure
 typedef struct{
         int size = 10;
         int front;
@@ -8,14 +19,28 @@ typedef struct{
 }Queue;
 
 Queue *CreateStringQueue(int size){
-	Queue* q = (Queue*)malloc(sizeof(size));
+	Queue* q = (char*)malloc(sizeof(size));
 	q->front = 0;
         q->rear = -1;
         return q;
 
 }
 void EnqueueString(Queue *q, char *string){
-	if(q
+	sem_post(&mutex);	
+	//if queue is full block til space is available
+	if(isFull()){
+		sem_wait(&wait);
+	}
+	//places pointer to the string at end of the queue
+	q->rear = string;
+	sem_wait(&mutex);
+	sem_post(&wait);
 }
-char * DequeueString(Queue *q){}
+char * DequeueString(Queue *q){
+	sem_post(&mutex);
+	if(!isFull()){
+		
+	}
+	sem_wait(&mutex);
+}
 void PrintQueueStats(Queue *q){}
