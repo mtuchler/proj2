c557aaea2cbfdb40c417d0ab69e5b043edaef33f
Abhishek Kosuri <kosuri@rockhopper-04.cs.wisc.edu>
2018-10-17 22:46:18 -0500

    writer

--- writer.c
+++ writer.c
@@ -12,7 +12,15 @@
 #include "threads.h"
 
 void* writer() {
+	char buff[BUFF_SIZE];
+	while(1){
+		strcpy(buff, DequeueString(Q[2]));
+		if(buff == NULL){
+			exit(1);
+		}
+		else{
+			//write to standard output
 
-	printf("writer\n");
+		}
 	return NULL;
 }
